3cd8ac5c8835cc69b4bd7f6c7a01cb5cfd411b0d
晁高锋 <chaogaofeng@avic-intl.cn>
2024-11-15 16:50:52 +0800

    fix YUV_420_888

--- h265.py
+++ h265.py
@@ -67,13 +67,16 @@ if __name__ == '__main__':
 
 	height, width = yuv_420_888.shape[:2]
 	# 使用 FFmpeg 压缩图像为 H.265 格式，并将输出流重定向到内存
-	stdout, stderr = (
-		ffmpeg
-		.input('pipe:0', format='rawvideo', pix_fmt='yuv420p', s=f'{width}x{height}')  # 指定输入的格式、像素格式和分辨率
-		.output('pipe:1', vcodec='libx265', format='hevc', pix_fmt='yuv420p')  # 指定输出格式为 H.265 和 YUV420p
-		.run(input=yuv_420_888.tobytes(), capture_stdout=True, capture_stderr=True)
-	)
-	print(stdout)
-	print(stderr)
+	try:
+		stdout, stderr = (
+			ffmpeg
+			.input('pipe:0', format='rawvideo', pix_fmt='yuv420p', s=f'{width}x{height}')  # 指定输入的格式、像素格式和分辨率
+			.output('pipe:1', vcodec='libx265', format='hevc', pix_fmt='yuv420p')  # 指定输出格式为 H.265 和 YUV420p
+			.run(input=yuv_420_888.tobytes(), capture_stdout=True, capture_stderr=True)
+		)
+		print(stdout)
+		print(stderr)
+	except ffmpeg.Error as e:
+		print(f"H.265 压缩失败: {e.stderr.decode('utf-8')}")
 
 
