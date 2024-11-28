2bade4fe75216082dead323401cf90ad227cb66a
晁高锋 <chaogaofeng@avic-intl.cn>
2024-11-28 18:38:00 +0800

    fix send

--- facefusion/api.py
+++ facefusion/api.py
@@ -57,7 +57,7 @@ def encode_h265(data, width, height, pix_fmt='yuv420p', vcodec='libx265'):
 		process = (
 			ffmpeg
 			.input('pipe:0', format='rawvideo', pix_fmt=pix_fmt, s=f'{width}x{height}')  # 指定输入的格式、像素格式和分辨率
-			.output('pipe:1', vcodec=vcodec, format='hevc', pix_fmt='y420p',
+			.output('pipe:1', vcodec=vcodec, format='hevc', pix_fmt='yuv420p',
 					preset='ultrafast')  # 指定输出格式为 H.265 和 YUV420p
 			.run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
 		)
